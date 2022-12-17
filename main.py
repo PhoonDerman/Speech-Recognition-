3642e6c19522682b004a84cc604461efa370dd87
Vivek Pillai <84534969+PhoonDerman@users.noreply.github.com>
2022-12-17 19:13:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import speech_recognition
+import pyttsx3
+
+
+
+# In[ ]:
+
+
+recognizer=speech_recognition.Recognizer()
+
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            text=recognizer.recognize_google(audio)
+            text=text.lower()
+            print(f"Recognized {text}")
+            
+    except speech_recognition.UnknownValueError():
+        recognizer=speech_recognition.Recognizer()
+
